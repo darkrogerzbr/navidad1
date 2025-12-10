85c52648930bec346e3a0dd10c272cba3ade54c5
darkrogerzbr <williamalvarez397@gmail.com>
2025-12-09 20:26:56 -0400

    9

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Danzas from './danzas.jsx'
 
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename='/navidad1'>
   <App>
+    <Danzas></Danzas>
   </App>
-  <Danzas></Danzas>
+  
   
   </BrowserRouter>,
 )
